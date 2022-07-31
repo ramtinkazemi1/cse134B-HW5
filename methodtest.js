5d084d345871cc42babb1fe8cf43b7f225a1b51b
ramtinkazemi1 <ramkazemi@yahoo.com>
2022-07-30 23:39:55 -0700

    wd

--- methodtest.js
+++ methodtest.js
@@ -1,59 +1,56 @@
-
 window.addEventListener(`DOMContentLoaded`, init);
+export function getFunc() {
+
+    const temp = new FormData(document.getElementById("test_form"));
+    const cst = new XMLHttpRequest();
+    cst.onload = function(){
+        document.getElementById("response").innerHTML = JSON.stringify(JSON.parse(cst.responseText), null, 4);
+    }
+    cst.open(`GET`, `https://httpbin.org/get`);
+    cst.send(temp);
+}
+
+
+export function postFunc(){
+
+    const temp = new FormData(document.getElementById("test_form"));
+    const cst = new XMLHttpRequest();
+
+    cst.onload = function() {
+        document.getElementById("response").innerHTML = JSON.stringify(JSON.parse(cst.responseText), null, 4);
+    }
+    cst.open("POST", "https://httpbin.org/post");
+    cst.send(temp);
+}
+
 function init(){
     document.getElementById("bt2").addEventListener("click",getFunc);
     document.getElementById("bt1").addEventListener("click",postFunc);
     document.getElementById("bt3").addEventListener("click",putFunc);
     document.getElementById("bt4").addEventListener("click",deleteFunc);
-    const today = new Date();
-    console.log(`Initialized at ${today}`);
 
-}
-
-export function getFunc() {
-
-    const temp = new FormData(document.getElementById("test_form"));
-    const xhr = new XMLHttpRequest();
-    xhr.onload = function(){
-        document.getElementById("response").innerHTML = JSON.stringify(JSON.parse(xhr.responseText), null, 4);
-    }
-    xhr.open(`GET`, `https://httpbin.org/get`);
-    xhr.send(temp);
-}
-
-
-export function postFunc(){
-    console.log(`time to post`);
-    const temp = new FormData(document.getElementById("test_form"));
-    const xhr = new XMLHttpRequest();
-
-    xhr.onload = function() {
-        document.getElementById("response").innerHTML = JSON.stringify(JSON.parse(xhr.responseText), null, 4);
-    }
-    xhr.open("POST", "https://httpbin.org/post");
-    xhr.send(temp);
 }
 
 export function putFunc(){
-    console.log(`time to put`);
-    const temp = new FormData(document.getElementById("test_form"));
-    const xhr = new XMLHttpRequest();
 
-    xhr.onload = function() {
-        document.getElementById("response").innerHTML = JSON.stringify(JSON.parse(xhr.responseText), null, 4);
+    const temp = new FormData(document.getElementById("test_form"));
+    const cst = new XMLHttpRequest();
+
+    cst.onload = function() {
+        document.getElementById("response").innerHTML = JSON.stringify(JSON.parse(cst.responseText), null, 4);
     }
-    xhr.open("PUT", "https://httpbin.org/put");
-    xhr.send(temp);
+    cst.open("PUT", "https://httpbin.org/put");
+    cst.send(temp);
 }
 
 export function deleteFunc(){
-    console.log(`time to delete`);
-    const temp = new FormData(document.getElementById("test_form"));
-    const xhr = new XMLHttpRequest();
 
-    xhr.onload = function() {
-        document.getElementById("response").innerHTML = JSON.stringify(JSON.parse(xhr.responseText), null, 4);
+    const temp = new FormData(document.getElementById("test_form"));
+    const cst = new XMLHttpRequest();
+
+    cst.onload = function() {
+        document.getElementById("response").innerHTML = JSON.stringify(JSON.parse(cst.responseText), null, 4);
     }
-    xhr.open("DELETE", "https://httpbin.org/delete");
-    xhr.send(temp);
+    cst.open("DELETE", "https://httpbin.org/delete");
+    cst.send(temp);
 }
