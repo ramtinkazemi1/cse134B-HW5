a78e40363b93d543be79ecc310ca4aa1ae2c131b
ramtinkazemi1 <ramkazemi@yahoo.com>
2022-07-30 23:51:41 -0700

    fvds

--- methodtest.js
+++ methodtest.js
@@ -1,26 +1,26 @@
 window.addEventListener(`DOMContentLoaded`, init);
 export function getFunc() {
 
-    const temp = new FormData(document.getElementById("test_form"));
+    const mvt = new FormData(document.getElementById("test_form"));
     const cst = new XMLHttpRequest();
     cst.onload = function(){
         document.getElementById("response").innerHTML = JSON.stringify(JSON.parse(cst.responseText), null, 4);
     }
     cst.open(`GET`, `https://httpbin.org/get`);
-    cst.send(temp);
+    cst.send(mvt);
 }
 
 
 export function postFunc(){
 
-    const temp = new FormData(document.getElementById("test_form"));
+    const mvt = new FormData(document.getElementById("test_form"));
     const cst = new XMLHttpRequest();
 
     cst.onload = function() {
         document.getElementById("response").innerHTML = JSON.stringify(JSON.parse(cst.responseText), null, 4);
     }
     cst.open("POST", "https://httpbin.org/post");
-    cst.send(temp);
+    cst.send(mvt);
 }
 
 function init(){
@@ -33,24 +33,24 @@ function init(){
 
 export function putFunc(){
 
-    const temp = new FormData(document.getElementById("test_form"));
+    const mvt = new FormData(document.getElementById("test_form"));
     const cst = new XMLHttpRequest();
 
     cst.onload = function() {
         document.getElementById("response").innerHTML = JSON.stringify(JSON.parse(cst.responseText), null, 4);
     }
     cst.open("PUT", "https://httpbin.org/put");
-    cst.send(temp);
+    cst.send(mvt);
 }
 
 export function deleteFunc(){
 
-    const temp = new FormData(document.getElementById("test_form"));
+    const mvt = new FormData(document.getElementById("test_form"));
     const cst = new XMLHttpRequest();
 
     cst.onload = function() {
         document.getElementById("response").innerHTML = JSON.stringify(JSON.parse(cst.responseText), null, 4);
     }
     cst.open("DELETE", "https://httpbin.org/delete");
-    cst.send(temp);
+    cst.send(mvt);
 }
